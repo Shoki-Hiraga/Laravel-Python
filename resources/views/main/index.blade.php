ab9ebf9489ede32f3fcc1e4490f5c2ab65ac364b
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-01-05 17:38:52 +0900

    初回コミット

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $python_title }}</title>
+</head>
+<body>
+    <h1>{{$python_h1}}</h1>
+    <p></p>
+</body>
+</html>
