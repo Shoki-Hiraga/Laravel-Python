449668130553013b63d8b63c458f642b0ca7fe82
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-01-12 02:18:59 +0900

    https://laravel-python.332web.com/ 本番公開、リファクタリング

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -8,7 +8,9 @@
     <title>{{ $python_title }}</title>
 </head>
 <body>
-    <h1>{{$python_h1}}</h1>
-    <p></p>
+    <h1>bladeファイルからテキストベタ打ち </h1>
+    <h2>以下はpyファイルから出力</h2>
+    <p>{{ $python_title }}</p>
+    <p>{{$python_h1}}</p>
 </body>
 </html>
