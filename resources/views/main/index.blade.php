4d3966b59ed8cfc03d2ed9445b912ede0c087d38
shoki-hiraga <chaser.cb750@gmail.com>
2025-03-13 11:21:32 +0900

    変更

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -20,6 +20,18 @@
     <h2>テスト入力フォームへ</h2>
     </a>   
 
+    <h2>twitter埋め込みテスト</h2>
+        <style>
+        .twitter_embed {
+            display: flex;
+            justify-content: center;  /* 横方向中央寄せ */
+            align-items: center;      /* 縦方向中央寄せ（必要なら） */
+        }
+        </style>
+    <div class="twitter_embed">
+        <blockquote class="twitter-tweet"><p lang="ja" dir="ltr">数日限定入庫車速報<br>クラウン アスリートV<br>GH-JZS171改<br>2000年<br>13.2万km<br>5MT<br>修復歴あり<br>総額241.4万円(税込)<br>ユーザー様買取車⭐︎90マークⅡ 5MT⭐︎BBS 20インチAW⭐︎社外足回り⭐︎1JZ ターボ⭐︎フジツボマフラー<a href="https://twitter.com/hashtag/%E8%BB%8A%E5%A3%B2%E3%82%8A%E3%81%BE%E3%81%99?src=hash&amp;ref_src=twsrc%5Etfw">#車売ります</a> <a href="https://t.co/L64pAXZV10">pic.twitter.com/L64pAXZV10</a></p>&mdash; ガレージカレント直販センタ (@GC__direct) <a href="https://twitter.com/GC__direct/status/1897151110001091017?ref_src=twsrc%5Etfw">March 5, 2025</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
     <h2>テストテキスト</h2>
 
 <p>
