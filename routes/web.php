887ae70d3acd0de6581921d6ad680265652c4e4c
shoki-hiraga <chaser.cb750@gmail.com>
2025-11-06 19:01:33 +0900

    管理画面導入

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PythonController;
 use App\Http\Controllers\FormController;
@@ -10,27 +11,22 @@ use App\Http\Controllers\PurchaseResultsImageController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
+// ---- Public pages ----
 Route::get('/', function () {
-    return view('main.index');
+    return view('main.index'); // ← TOPはこっちを採用
 })->name('TOP');
 
-Route::get('/test', [PythonController::class, 'index'])
-->name('test');
+Route::get('/test', [PythonController::class, 'index'])->name('test');
 
 Route::get('/aws_tran', function () {
     return view('main.AWS_transfer');
 })->name('aws_tran');
 
-Route::get('/aws_opt', function () {
+Route::get('/aws_opt', function () {          // ← name を "aws_opt" に修正
     return view('main.AWS_optimization');
-})->name('aaws_opt');
+})->name('aws_opt');
 
 Route::get('/q_top', function () {
     return view('main.q_top');
@@ -40,19 +36,33 @@ Route::get('/q_top/historia-article', function () {
     return view('main.q_historia');
 })->name('historia_article');
 
+// ---- Form ----
 Route::get('/form', [FormController::class, 'showForm'])->name('form');
 Route::post('/form', [FormController::class, 'submitForm']);
 
-Route::get('/images', [ImageController::class, 'index'])
-->name('images');
-
+// ---- Image Processing ----
+Route::get('/images', [ImageController::class, 'index'])->name('images');
 Route::post('/images/upload', [ImageController::class, 'upload']);
 Route::post('/images/process', [ImageController::class, 'process']);
 Route::get('/images/download/{id}', [ImageController::class, 'download']);
 Route::delete('/images/{id}', [ImageController::class, 'destroy'])->name('images.destroy');
 
+// ---- Purchase Result Image ----
 Route::get('/purchase-results-images', [PurchaseResultsImageController::class, 'index'])->name('purchase_results_images.index');
 Route::get('/purchase-results-images/create', [PurchaseResultsImageController::class, 'create'])->name('purchase_results_images.create');
 Route::get('/purchase-results-images/{id}/download', [PurchaseResultsImageController::class, 'download'])->name('purchase_results_images.download');
 Route::delete('/purchase-results-images/{id}', [PurchaseResultsImageController::class, 'destroy'])->name('purchase_results_images.destroy');
 Route::post('/purchase-results-images', [PurchaseResultsImageController::class, 'store'])->name('purchase_results_images.store');
+
+// ---- Auth Required (Breeze) ----
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
