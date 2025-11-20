f11e02c503cbd6dabda332d1832e0ff3706fa966
shoki-hiraga <chaser.cb750@gmail.com>
2025-11-20 12:10:26 +0900

    rootのoutputフォルダ指定をできるように追加

--- app/Python/setting_file/csv_output_path.py
+++ app/Python/setting_file/csv_output_path.py
@@ -1,8 +1,24 @@
+import os
+
+# ▼ 今のファイル(csv_output_path.py)の絶対パス
+CURRENT_FILE = os.path.abspath(__file__)
+
+# ▼ setting_file フォルダの親 → プロジェクトのルートと想定
+PROJECT_ROOT = os.path.dirname(os.path.dirname(CURRENT_FILE))
+
+# ▼ ルート直下に output フォルダを作る
+out_root = os.path.join(PROJECT_ROOT, "output")
+
+
+# ---- 以下はあなたの既存データ ----
 in_office = 'C:/Users/hiraga/Downloads'
 out_office = 'C:/Users/hiraga/Downloads'
+
 in_a09 = 'C:/Users/main/Downloads'
 out_main = 'C:/Users/main/Downloads'
+
 in_raytrek = 'C:/Users/RAYTREK/Downloads'
 out_raytrek = 'C:/Users/RAYTREK/Downloads'
+
 in_K39_sho = 'C:/Users/K39_sho/Downloads'
 out_K39_sho = 'C:/Users/K39_sho/Downloads'
--- app/Python/setting_file/file_path.py
+++ app/Python/setting_file/file_path.py
@@ -1,12 +1,13 @@
 from setting_file import csv_output_path
 
 # ファイルパス
-file_directory_index = 1 # 使用するファイルパスのインデックス番号
+file_directory_index = 5 # 使用するファイルパスのインデックス番号
 file_Directorys = {
     1: csv_output_path.out_office,
     2: csv_output_path.out_main,
     3: csv_output_path.out_raytrek,
-    4: csv_output_path.out_K39_sho
+    4: csv_output_path.out_K39_sho,
+    5: csv_output_path.out_root
 }
 
 file_directory = file_Directorys[file_directory_index]
